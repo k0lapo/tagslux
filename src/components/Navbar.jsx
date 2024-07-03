ce48cfdc7f8ed9499f52ec694ffa0b04ceb0e3a7
Adegbite Kolapo <madegbite7@gmail.com>
2024-07-03 21:40:13 +0100

    updated navbar icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart, FaBars, FaTimes, FaUser, FaInstagram, FaTiktok, FaWhatsapp } from 'react-icons/fa';
 import { useCart } from '../context/CartContext';
-import {FaXTwitter} from 'react-icons/fa6';
+import { FaXTwitter } from 'react-icons/fa6';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -38,8 +38,7 @@ const Navbar = () => {
               </span>
             )}
           </Link>
-          
-            <FaUser />       
+          <FaUser />
         </div>
         <div className="md:hidden flex items-center">
           <button onClick={toggleMenu} className="text-black focus:outline-none">
@@ -60,7 +59,7 @@ const Navbar = () => {
           <Link to="/" onClick={closeMenu} className="block py-2 text-black text-xl hover:text-gray-700 font-bold">Home</Link>
           <Link to="/product" onClick={closeMenu} className="block py-2 text-black text-xl hover:text-gray-700 font-bold">Our Store</Link>
           <Link to="/about" onClick={closeMenu} className="block py-2 text-black text-xl hover:text-gray-700 font-bold">About</Link>
-          <div className='flex space-x-6 mt-6'>
+          <div className="flex space-x-6 mt-6">
             <Link to="/cart" onClick={closeMenu} className="relative block py-2 text-black text-xl hover:text-gray-700 font-bold">
               <FaShoppingCart className="inline" />
               {cartItemCount > 0 && (
@@ -69,23 +68,21 @@ const Navbar = () => {
                 </span>
               )}
             </Link>
-            
             <FaUser className="inline" />
-            
           </div>
         </div>
         {/* Social media icons section for mobile */}
-        <div className={`md:hidden flex justify-center space-x-4 mb-10 ${isOpen ? 'flex' : 'hidden'}`}>
+        <div className="md:hidden flex justify-center space-x-4 mb-10">
           <a href="https://instagram.com/tagslux" className="text-black font-bold">
             <FaInstagram size={24} />
           </a>
           <a href="https://www.tiktok.com/@tagslux?is_from_webapp=1&sender_device=pc" className="font-bold text-black">
             <FaTiktok size={24} />
           </a>
-          <a href="https://wa.me/07013306431" className=" font-bold text-black">
+          <a href="https://wa.me/07013306431" className="font-bold text-black">
             <FaWhatsapp size={24} />
           </a>
-          <a href="https://twitter.com/tagslux" className=" font-bold text-black">
+          <a href="https://twitter.com/tagslux" className="font-bold text-black">
             <FaXTwitter size={24} />
           </a>
         </div>
